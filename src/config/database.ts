f74634ae8e96616909b8bbf59eb36ee337f8f877
Bill Radjewski <radjewwj@gmail.com>
2024-06-17 15:45:44 -0400

    kysely config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,10 +1,12 @@
 import { Promise } from 'bluebird';
 import pgp from 'pg-promise';
+import { Pool } from 'pg';
+import { Kysely, PostgresDialect } from 'kysely';
 
 const user = process.env.DATABASE_USER;
 const password = process.env.DATABASE_PASSWORD;
 const host = process.env.DATABASE_HOST;
-const port = process.env.DATABASE_PORT;
+const port = process.env.DATABASE_PORT || '5432';
 const dbName = process.env.DATABASE;
 
 const authDbHost = process.env.AUTH_DATABASE_HOST;
@@ -17,3 +19,20 @@ const authConnectionString = `postgres://${authDbUser}:${authDbPassword}@${authD
 
 export const db = pgp({ promiseLib: Promise })(connectionString);
 export const authDb = pgp({ promiseLib: Promise })(authConnectionString);
+
+import { DB } from './types/db';
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: dbName,
+    host,
+    port: parseInt(port),
+    user,
+    password,
+    max: 10,
+  })
+});
+
+export const kdb = new Kysely<DB>({
+  dialect,
+});
