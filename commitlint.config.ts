70ca49c3aa1632c2663de8087a512a77278ff9d0
Bill Radjewski <radjewwj@gmail.com>
2024-08-18 12:00:34 -0400

    fix: ignore release commits

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,6 +3,7 @@ import type { UserConfig } from '@commitlint/types';
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   formatter: '@commitlint/format',
+  ignores: [(message) => message.startsWith('chore(release)')],
 };
 
 export default Configuration;
