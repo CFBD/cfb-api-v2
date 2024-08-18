486dfec1e76b896d3aafe9df48a3c438e7ac18e6
Bill Radjewski <radjewwj@gmail.com>
2024-08-17 22:25:03 -0400

    quota updates

--- src/config/middleware/quotas.ts
+++ src/config/middleware/quotas.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { authDb } from '../database';
 
+const ignoredPaths = ['/live/plays', '/games/weather', '/scoreboard'];
+
 interface ApiUser {
   id: number;
   username: string;
@@ -10,7 +12,7 @@ interface ApiUser {
 
 export const checkCallQuotas = async (req: Request, res: Response, next: NextFunction) => {
   // @ts-ignore
-  if (req.user && req.user.remainingCalls <= 0) {
+  if (req.user && !ignoredPaths.includes(req.path) && req.user.remainingCalls <= 0) {
     res.status(429).send({
       message: 'Monthly call quota exceeded.',
     });
@@ -25,23 +27,25 @@ export const updateQuotas = async (req: Request, res: Response, next: NextFuncti
 
   // @ts-ignore
   res.send = async (body) => {
-    if (res.statusCode === 200 && req.user) {
+    if (res.statusCode === 200 && req.user && !ignoredPaths.includes(req.path)) {
       const user = req.user as ApiUser;
 
       if (res.statusCode === 200) {
-        user.remainingCalls -= 1;
-
         try {
-          await authDb.none(
-            `UPDATE "user" SET remaining_calls = $1 WHERE id = $2`,
-            [user.remainingCalls, user.id],
+          const remaining = await authDb.one(
+            `UPDATE "user" SET remaining_calls = (remaining_calls - 1) WHERE id = $1 RETURNING remaining_calls`,
+            [user.id],
           );
+          user.remainingCalls = remaining.remaining_calls;
         } catch (error) {
           console.error('Error updating remaining calls', error);
         }
-
-        res.setHeader('X-CallLimit-Remaining', user.remainingCalls);
       }
+    }
+
+    if (req.user) {
+      // @ts-ignore
+      res.setHeader('X-CallLimit-Remaining', req.user.remainingCalls);
     }
 
     temp.call(res, body);
