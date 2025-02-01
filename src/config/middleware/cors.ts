bab13c009c72e136d2843fd18f9e5023dbeaa9a0
Bill Radjewski <radjewwj@gmail.com>
2025-02-01 10:26:27 -0500

    chore: disable cors

--- src/config/middleware/cors.ts
+++ src/config/middleware/cors.ts
@@ -1,24 +1,21 @@
 import cors from 'cors';
 
-const env = process.env.NODE_ENV;
+// const env = process.env.NODE_ENV;
 const corsOrigin = process.env.CORS_ORIGIN || 'https://collegefootballdata.com';
 console.log(`CORS Origin: ${corsOrigin}`);
 
 let corsOptions: cors.CorsOptions = {};
 
-if (env != 'development') {
-  corsOptions = {
-    origin: (requestOrigin, callback) => {
-      console.log(`Request Origin: ${requestOrigin}`);
-      if (!requestOrigin || requestOrigin == corsOrigin) {
-        console.log('Allowed by CORS', requestOrigin);
-        callback(null);
-      } else {
-        console.log('Not allowed by CORS', requestOrigin);
-        callback(new Error(`Not allowed by CORS: ${requestOrigin}`));
-      }
-    },
-  };
-}
+// if (env != 'development') {
+//   corsOptions = {
+//     origin: (requestOrigin, callback) => {
+//       if (!requestOrigin || requestOrigin == corsOrigin) {
+//         callback(null);
+//       } else {
+//         callback(new Error(`Not allowed by CORS: ${requestOrigin}`));
+//       }
+//     },
+//   };
+// }
 
 export default cors(corsOptions);
